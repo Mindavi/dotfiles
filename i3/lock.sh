1cdff55244d12d82133bbd51aa3da0f9b66dacd7
Rick van Schijndel <rol3517@gmail.com>
2018-05-17 10:33:58 +0200

    Higher blur, attribution to original author

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# from https://gist.github.com/csivanich/10914698
+
 # i3lock blurred screen inspired by /u/patopop007 and the blog post
 # http://plankenau.com/blog/post-10/gaussianlock
 
@@ -22,7 +24,8 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
 #BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
-BLURTYPE="5x2" # 3.80s
+#BLURTYPE="5x2" # 3.80s
+BLURTYPE="0x8"
 #BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
 
