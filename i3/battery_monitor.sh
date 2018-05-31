1560b0be686c47ab00dc2fa6e41690d65d8805cd
Rick van Schijndel <rol3517@gmail.com>
2018-05-31 11:01:11 +0200

    Fix battery monitor script for my purpose, it should work now

--- i3/battery_monitor.sh
+++ i3/battery_monitor.sh
@@ -5,16 +5,17 @@ THRESHOLD=15 # percent
 
 lock_path='/tmp/batmon.lock'
 
+# sudo apt install procmail
 lockfile -r 0 $lock_path 2>/dev/null || exit
 
 acpi_path=$(find /sys/class/power_supply/ -name 'BAT*' | head -1)
-charge_now=$(cat "$acpi_path/charge_now")
-charge_full=$(cat "$acpi_path/charge_full")
+charge_now=$(cat "$acpi_path/energy_now")
+charge_full=$(cat "$acpi_path/energy_full")
 charge_status=$(cat "$acpi_path/status")
-charge_percent=$(printf '%.0f' $(echo "$charge_now / $charge_full * 100" | bc -l))
-message="Battery running critically low at $charge_percent%!"
+charge_percent=$(LC_NUMERIC="en_US.UTF-8" printf '%.0f' $(echo "$charge_now / $charge_full * 100" | bc -l))
 
 if [[ $charge_status == 'Discharging' ]] && [[ $charge_percent -le $THRESHOLD ]]; then
+  message="Battery running critically low at $charge_percent%!"
   DISPLAY=:0.0 /usr/bin/notify-send -u critical "Low battery" "$message"
 fi
 
