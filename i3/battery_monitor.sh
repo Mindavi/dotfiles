2612059a7c3c142facf8b28f196c66293b218d40
Rick van Schijndel <rol3517@gmail.com>
2018-12-19 11:01:49 +0100

    Battery monitor will not spam notifications anymore, it should just show one

--- i3/battery_monitor.sh
+++ i3/battery_monitor.sh
@@ -15,8 +15,8 @@ charge_status=$(cat "$acpi_path/status")
 charge_percent=$(LC_NUMERIC="en_US.UTF-8" printf '%.0f' $(echo "$charge_now / $charge_full * 100" | bc -l))
 
 if [[ $charge_status == 'Discharging' ]] && [[ $charge_percent -le $THRESHOLD ]]; then
-  message="Battery running critically low at $charge_percent%!"
-  DISPLAY=:0.0 /usr/bin/notify-send -u critical "Low battery" "$message"
+  message="Battery running low at $charge_percent%!"
+  DISPLAY=:0.0 /usr/bin/notify-send -t 1000 -u critical "Low battery" "$message"
 fi
 
 rm -f $lock_path
